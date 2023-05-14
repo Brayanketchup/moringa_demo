cbb8ff3298d37c287d56aa4594045e35455efcf2
Brayanketchup <brayanmartinez1102@gmail.com>
2023-05-13 22:43:59 -0400

    slides section

--- src/script.js
+++ src/script.js
@@ -5,12 +5,12 @@
 
 // on load set time out to make the loading img vanish
 window.onload = function(loaded){
-    setTimeout(() => {loadimg.style.opacity = 0 },2000);
-    loaded();
+  setTimeout(() => {loadimg.style.opacity = 0 },2000);
+  loaded();
 }
 // once the animation is done, set the img display to none
 function loaded(){
-    loadimg.style.display = none;
+  loadimg.style.display = none;
 ;}
 
 const hamburgerMenu = document.querySelector('.hamburger');
@@ -18,8 +18,8 @@ const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.mobile_display_overlay');
 
 hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('active');
-    menu.classList.toggle('active')
-    overlay.classList.toggle('active');
+  hamburgerMenu.classList.toggle('active');
+  menu.classList.toggle('active')
+  overlay.classList.toggle('active');
 })
 
